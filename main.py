5fccd6536ff082cddcff2c6f9e194c0b49e8a0dc
Matei-oss <andrei_mateiu1996@live.com>
2020-03-06 21:26:56 +0000

    added only over 100 points feature

--- main.py
+++ main.py
@@ -12,7 +12,7 @@
 
 import requests
 from bs4 import BeautifulSoup
-
+import pprint
 res = requests.get('https://news.ycombinator.com/news') # think of this as a browser without a window
 
 # print(res.text) # prints the entire html file
@@ -23,12 +23,28 @@ soup_object = BeautifulSoup(res.text, 'html.parser')
 
 #print(soup_object.select('.storylink')[0]) #css selector , we can also type #id and the element
 
-links = soup_object.select('storylink')
+links = soup_object.select('.storylink')
 
-votes = soup_object.select('.score')
-print(votes[0])
+subtext = soup_object.select('.subtext')
+#print(votes[0])
 
 # we can specify which object to show in []
 
-print(votes[0].get('id'))
+#print(votes[0].get('id'))
 
+def create_custom_hn(links, subtext):
+  hn = []
+  for idx, item in enumerate(links):
+    title = item.getText()
+    href = item.get('href', None)
+    vote = subtext[idx].select('.score')
+    if len(vote):
+      points = int(vote[0].getText().replace('points',''))
+      if points > 99:
+        hn.append({'title': title,'link': href,'votes': points})
+  return hn 
+
+pprint.pprint(create_custom_hn(links, subtext))
+
+
+ 
